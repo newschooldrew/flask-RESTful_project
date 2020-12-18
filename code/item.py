eb744f78edb94880952154525b9624fa80852879
Andrew Perez <andrewperez@Andrews-MacBook-Air.local>
2020-12-18 09:39:22 -0800

    cleanup

---
+++ code/item.py
@@ -0,0 +1,84 @@
+import sqlite3
+from flask_restful import Resource, reqparse
+from flask_jwt import jwt_required
+
+items = []
+
+
+class Item(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument('price',
+                        type=float,
+                        required=True,
+                        help='this field cant bbe left blank')
+
+    @jwt_required()
+    def get(self, name):
+        item = self.find_by_name(name)
+        if item:
+            return item
+        return {'message':'Item not found'}, 404
+        # next gives us the first item in this filter function
+        # placing None in next(), gives the function a default
+
+        # for item in items:
+        #     if item['name'] == name:
+        #         return item
+        # return {'item': item}, 200 if item else 404
+
+    @classmethod
+    def find_by_name(cls,name):
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+
+        query = "SELECT * FROM items WHERE name=?"
+        result = cursor.execute(query,(name,))
+        row = result.fetchone()
+        connection.close()
+
+        if row:
+            return {'item':{'name':row[0],'price':row[1]}}
+
+    def post(self, name):
+        # if next(filter(lambda item: item['name'] == item, items), None):
+        if self.find_by_name(name):
+            return {"message": "An item with name '{}' already exists.".format(name)}, 400
+
+        data = Item.parser.parse_args()
+
+        item = {'name': name, 'price': data['price']}
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+        query = "INSERT INTO items values(?,?)"
+
+        cursor.execute(query,(item['name'],item['price']))
+        connection.commit()
+        connection.close()
+        return item, 201
+
+    def delete(self, name):
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+
+        query = "DELETE FROM items WHERE NAME =?"
+        cursor.execute(query,(name,))
+
+        connection.commit()
+        connection.close()
+
+        return {'message': 'item deleted'}
+
+    def put(self, name):
+        data = Item.parser.parse_args()
+        item = next(filter(lambda x: x['name'] == name, items), None)
+        if item is None:
+            item = {'name': name, 'price': data['price']}
+            items.append(item)
+        else:
+            item.update(data)
+        return item
+
+
+class ItemList(Resource):
+    def get(self):
+        return {'items': items}
